51cd281e5516ffd68b7a6b078849bef1351fde7f
swtt-pch <igor.gsouza@bandtec.com.br>
2022-10-10 21:33:09 -0300

    feat: Lifecycle of a Bean - @PostConstruct and @PreDestroy :sparkles:

--- src/main/java/com/student/level1/basic/BinarySearchImpl.java
+++ src/main/java/com/student/level1/basic/BinarySearchImpl.java
@@ -1,24 +1,24 @@
 package com.student.level1.basic;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearchImpl {
-    // Optional - use setter injection
-    // otherwise use constructor injection
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     @Qualifier("bubble")
     private SortAlgorithm sortAlgorithm;
-
-//    @Autowired // dependecy injection using name
-//    private SortAlgorithm bubbleSortAlgorithm;
-
-    // 1- Sorting an array
     public int binarySearch(int[] numbers, int numberToSearchFor){
         // 1.1 - Implement Sorting Logic, so you had to choose
         int [] sortedNumbers = sortAlgorithm.sort(numbers);
@@ -28,4 +28,12 @@ public class BinarySearchImpl {
         return 3;
     }
 
+    @PostConstruct
+    public void postConstruct(){
+        logger.info("postConstruct");
+    }
+    @PreDestroy
+    public void preDestroy(){
+        logger.info("PreDestroy");
+    }
 }
