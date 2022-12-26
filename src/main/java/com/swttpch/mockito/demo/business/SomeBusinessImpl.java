b52f52ee67a27c83029325ce63e718c8cd307b20
swtt-pch <i.greygomes3@gmail.com>
2022-12-26 20:13:33 -0300

    feat: Understanding problems with Stubs 👎

--- src/main/java/com/swttpch/mockito/demo/business/SomeBusinessImpl.java
+++ src/main/java/com/swttpch/mockito/demo/business/SomeBusinessImpl.java
@@ -3,6 +3,10 @@ package com.swttpch.mockito.demo.business;
 public class SomeBusinessImpl {
     private DataService dataService;
 
+    public SomeBusinessImpl(DataService dataService) {
+        this.dataService = dataService;
+    }
+
     public int findTheGreatestFromAllData() {
         int[] data = dataService.retrieveAllData();
         int greatestValue = Integer.MIN_VALUE;
