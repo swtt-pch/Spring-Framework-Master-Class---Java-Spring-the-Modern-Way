8c64098b0ec73a64511bb14b1e1c19473a51a65c
swtt-pch <i.greygomes3@gmail.com>
2022-12-26 20:25:54 -0300

    feat: Simplifying Tests with Mockito Annotations - @Mock, @InjectMocks

--- src/test/java/com/swttpch/mockito/demo/business/SomeBusinessImplMockTest.java
+++ src/test/java/com/swttpch/mockito/demo/business/SomeBusinessImplMockTest.java
@@ -1,28 +1,35 @@
 package com.swttpch.mockito.demo.business;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class SomeBusinessImplMockTest {
-
+    @Mock
+    private DataService dataService;
+    @InjectMocks
+    private SomeBusinessImpl someBusiness;
     @Test
     void findTheGreatestFromAllData_basicScenario() {
-        DataService dataService = mock(DataService.class);
-//        dataService.retrieveAllData() => new int[] {1,2,3};
         when(dataService.retrieveAllData()).thenReturn(new int[] {1,2,3});
-        SomeBusinessImpl someBusiness = new SomeBusinessImpl(dataService);
         assertEquals(3, someBusiness.findTheGreatestFromAllData());
     }
     @Test
     void findTheGreatestFromAllData_withOneValue() {
-        DataService dataService = mock(DataService.class);
-//        dataService.retrieveAllData() => new int[] {1,2,3};
         when(dataService.retrieveAllData()).thenReturn(new int[] {1});
-        SomeBusinessImpl someBusiness = new SomeBusinessImpl(dataService);
-        int result = someBusiness.findTheGreatestFromAllData();
-        assertEquals(1, result);
+        assertEquals(1, someBusiness.findTheGreatestFromAllData());
+    }
+
+    @Test
+    void findTheGreatestFromAllData_emptyArray() {
+        when(dataService.retrieveAllData()).thenReturn(new int[] {});
+        assertEquals(Integer.MIN_VALUE, someBusiness.findTheGreatestFromAllData());
     }
 }
