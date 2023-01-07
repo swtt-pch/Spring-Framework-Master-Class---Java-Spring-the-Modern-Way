0992977a908d2b1302b7de16d2ee444839a24123
swtt-pch <i.greygomes3@gmail.com>
2023-01-07 16:37:05 -0300

    feat: Build a Hello World API with Spring Boot

---
+++ src/main/java/com/swttpch/learningspringboot/controller/CourseController.java
@@ -0,0 +1,20 @@
+package com.swttpch.learningspringboot.controller;
+
+import com.swttpch.learningspringboot.entity.Course;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses() {
+        return Arrays.asList(
+                new Course(1, "Learn AWS", "in28Minutes"),
+                new Course(1, "Learn DevOps", "in28Minutes")
+        );
+    }
+}
